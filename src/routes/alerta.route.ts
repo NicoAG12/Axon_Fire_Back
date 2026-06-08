21860887f6ae986217a8baf91d2f6c6422958292
NicoAG12 <73404339+NicoAG12@users.noreply.github.com>
2026-06-08 13:52:13 -0300

    se cambio de post a get

--- src/routes/alerta.route.ts
+++ src/routes/alerta.route.ts
@@ -7,7 +7,7 @@ const alertaController = new AlertaController();
 
 router.post('/crear', verificarHeaders, alertaController.crearAlerta);
 router.post('/crear-con-notificacion', verificarHeaders, alertaController.crearAlertaYNotificar);
-router.post('/rango', verificarHeaders, alertaController.obtenerAlertasPorFecha)
+router.get('/rango', verificarHeaders, alertaController.obtenerAlertasPorFecha)
 router.get('/usuario/:id_usuario', verificarHeaders, alertaController.obtenerAlertasPorUsuario)
 router.get('/:id_alerta', verificarHeaders, alertaController.obtenerAlertaPorID);
 router.patch('/:id_alerta/finalizar', verificarHeaders, verificarRolAdmin, alertaController.finalizarAlerta);
