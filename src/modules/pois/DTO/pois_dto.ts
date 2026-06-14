69013db0013669c7cc02a2206f54dc4df9bce170
NicoAG12 <73404339+NicoAG12@users.noreply.github.com>
2026-06-14 14:22:09 -0300

    mensaje en categoria poi

--- src/modules/pois/DTO/pois_dto.ts
+++ src/modules/pois/DTO/pois_dto.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 
 export const crearPoiSchema = z.object({
     nombre: z.string().min(1, "El nombre no puede estar vacío"),
-    categoria: z.nativeEnum(categoria_poi),
+    categoria: z.nativeEnum(categoria_poi, { 
+        error: `Categoria invalida. Debe ser una de: ${Object.values(categoria_poi).join(', ')}` 
+    }),
     descripcion: z.string().optional(),
     latitud: z.number().min(-90, "La latitud debe estar entre -90 y 90").max(90, "La latitud debe estar entre -90 y 90"),
     longitud: z.number().min(-180, "La longitud debe estar entre -180 y 180").max(180, "La longitud debe estar entre -180 y 180")
