85fb0732b171533f5a02d01f6fabba781c4be522
lauti23sz <lautiszlosberg@gmail.com>
2026-06-29 11:53:55 -0300

    misc

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/tests/examen_fin/'],
   verbose: true,
   forceExit: true,
   clearMocks: true,
